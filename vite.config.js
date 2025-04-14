4e8dcf791f62eb635a137d61f72be038cddc11f1
docoi <142594694+docoi@users.noreply.github.com>
2025-04-14 20:39:55 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  root: '.', // explicitly set root to current directory
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: path.resolve(__dirname, 'index.html'),
+    },
+  },
 });
