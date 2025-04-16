3cf699b677a32c1e25cdf9dd2c629f438daa532e
docoi <trackingdata2020@gmail.com>
2025-04-16 18:01:23 +0100

    Simplify vite.config.js for Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  root: '.', // explicitly set root to current directory
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      input: path.resolve(__dirname, 'index.html'),
-    },
   },
 });
