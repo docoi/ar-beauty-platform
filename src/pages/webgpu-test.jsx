6dbeaf3a973f5cf7618cde2d97b40e711aa8a94c
docoi <trackingdata2020@gmail.com>
2025-05-02 13:27:04 -0400

    WebGL install

---
+++ src/pages/webgpu-test.jsx
@@ -0,0 +1,9 @@
+import WebGPUDemo from '@/components/WebGPUDemo';
+
+export default function WebGPUPage() {
+  return (
+    <div className="w-full h-screen">
+      <WebGPUDemo />
+    </div>
+  );
+}
