59e465157c8afe773f24bbfa3ffc16c91cc08689
docoi <trackingdata2020@gmail.com>
2025-05-06 17:54:52 -0400

    WebGPU DemoMobile

--- src/pages/LipstickMirrorLive.jsx
+++ src/pages/LipstickMirrorLive.jsx
@@ -48,7 +48,7 @@ const MirrorLive = () => {
 
   return (
     <div className="w-full h-screen flex flex-col items-center justify-center">
-      <video ref={videoRef} autoPlay playsInline muted className="rounded-xl shadow-lg mb-4" />
+      <video ref={videoRef} className="hidden" autoPlay playsInline muted />
       <canvas ref={canvasRef} className="w-full h-full" />
     </div>
   );
