734ab2807b84b35fe578e8afb6c4f10055800623
docoi <trackingdata2020@gmail.com>
2025-05-13 19:11:14 -0400

    WebGPU Mim test page

--- src/App.jsx
+++ src/App.jsx
@@ -46,8 +46,8 @@ function App() {
         <Route path="/webgpu-test" element={<WebGPUDemo />} />
         <Route path="/lipstick-mirror" element={<LipstickMirror />} />
         <Route path="/mirror" element={<MirrorPage />} />
-        <Route path="/mirror-live" element={<TestWebGPUCanvas />} />
         <Route path="/test-webgpu" element={<TestWebGPUCanvas />} />
+        <Route path="/mirror-live" element={<LipstickMirrorLive />} />
       </Routes>
     </Router>
   );
