33f24ca0047d16aec79c32ce2f3e24f2bf6d1595
docoi <trackingdata2020@gmail.com>
2025-06-17 07:54:41 -0400

    3D Lipstick gbl

--- src/pages/LipstickMirrorLive_Clone.jsx
+++ src/pages/LipstickMirrorLive_Clone.jsx
@@ -64,7 +64,7 @@ export default function LipstickMirrorLive_Clone() {
 
                 const scaleMatrix = mat4.create();
                 // This is the most important value. If still spiky, make it smaller.
-                const scaleFactor = 0.84; 
+                const scaleFactor = 1.5; 
                 mat4.fromScaling(scaleMatrix, vec3.fromValues(scaleFactor, scaleFactor, scaleFactor));
                 
                 // Correct order: Final Matrix = FacePose * Translation * Scale
